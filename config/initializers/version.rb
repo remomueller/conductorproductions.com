8df0ff8fbea57f527aca359c66bb52f95f9db5ff
Remo Mueller <remosm@gmail.com>
2015-04-18 11:13:36 -0400

    Version bump to 0.3.0.pre

---
+++ config/initializers/version.rb
@@ -0,0 +1,10 @@
+module WwwConductorproductionsCom
+  module VERSION #:nodoc:
+    MAJOR = 0
+    MINOR = 3
+    TINY = 0
+    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
+
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
+  end
+end
