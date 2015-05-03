15a6fb8489ea8f632877107e7202e27212c31146
Remo Mueller <remosm@gmail.com>
2015-05-03 13:02:36 -0400

    Simplified test helper to use pride instead of spec reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,5 @@
 require 'simplecov'
-
-require 'minitest/autorun'
-require 'minitest/reporters'
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+require 'minitest/pride'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -10,9 +7,6 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
