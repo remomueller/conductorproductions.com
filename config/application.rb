bd37a2c07e7e2b30e2c0e49f3cbe02249be32315
Remo Mueller <remosm@gmail.com>
2015-06-24 00:06:30 -0400

    Updated styling across several pages.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module WwwConductorproductionsCom
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| "<span class=\"has-error\">#{html_tag}</span>".html_safe }
   end
 end
