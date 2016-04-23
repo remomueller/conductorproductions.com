fcc2be8ca0d74d6b52e793930508ea06387b06e0
Remo Mueller <remosm@gmail.com>
2016-04-23 16:30:11 -0400

    Version 0.5.1 finalized

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.5.1
+## 0.5.1 (April 23, 2016)
 
 ### Enhancements
 - **Client Changes**
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,7 +5,7 @@ module WwwConductorproductionsCom
     MAJOR = 0
     MINOR = 5
     TINY = 1
-    BUILD = "beta1" # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    BUILD = nil # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
