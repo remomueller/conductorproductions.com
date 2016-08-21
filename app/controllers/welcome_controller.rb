696adc0f57bfb51d80510c0454edcc73efda6841
Remo Mueller <remosm@gmail.com>
2016-08-21 17:14:50 -0400

    Minor cleanup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 # Displays public facing pages.
 class WelcomeController < ApplicationController
-  before_action :authenticate_user!, only: [ :dashboard ]
+  before_action :authenticate_user!, only: [:dashboard]
 
   layout 'conductor-application-v2'
 
