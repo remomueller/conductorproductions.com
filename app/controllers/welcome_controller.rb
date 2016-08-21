753535a9b30aaf22bc08e6b9813ec1ca5a92380c
Remo Mueller <remosm@gmail.com>
2016-08-21 17:13:39 -0400

    Updated deliver_later to deliver_now

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,7 +8,7 @@ class WelcomeController < ApplicationController
 
   def submit_contact
     if params[:name].present? && params[:email].present? && params[:body].present?
-      UserMailer.contact(params[:name], params[:email], params[:body]).deliver_later if EMAILS_ENABLED
+      UserMailer.contact(params[:name], params[:email], params[:body]).deliver_now if EMAILS_ENABLED
       redirect_to contact_path, notice: 'Thanks for getting in touch!'
     else
       render 'contact'
