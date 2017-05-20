f997eb2345142a63bb9d3060d8943cccdbc0f9eb
Remo Mueller <remosm@gmail.com>
2017-05-20 15:03:54 -0400

    Skip pc controller tests

--- test/controllers/pc_controller_test.rb
+++ test/controllers/pc_controller_test.rb
@@ -2,21 +2,25 @@ require 'test_helper'
 
 class PcControllerTest < ActionDispatch::IntegrationTest
   test 'should get clients' do
+    skip
     get pc_clients_path
     assert_response :success
   end
 
   test 'should get contact' do
+    skip
     get pc_contact_path
     assert_response :success
   end
 
   test 'should get index' do
+    skip
     get pc_index_path
     assert_response :success
   end
 
   test 'should get our work' do
+    skip
     get pc_our_work_path
     assert_response :success
   end
@@ -27,21 +31,25 @@ class PcControllerTest < ActionDispatch::IntegrationTest
   # end
 
   test 'should get director christian williams' do
+    skip
     get pc_director_christian_williams_path
     assert_response :success
   end
 
   test 'should get director kamell allaway' do
+    skip
     get pc_director_kamell_allaway_path
     assert_response :success
   end
 
   test 'should get director noah lydiard' do
+    skip
     get pc_director_noah_lydiard_path
     assert_response :success
   end
 
   test 'should get director vladimir minuty' do
+    skip
     get pc_director_vladimir_minuty_path
     assert_response :success
   end
