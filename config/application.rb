c7028887f4cfaa35f48f49cb4b2cb84696bf748e
Remo Mueller <remosm@gmail.com>
2017-10-08 16:53:17 -0400

    Modified error message on contact page

--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,7 @@ module WwwConductorproductionsCom
     # Run "rails time:zones" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = "Eastern Time (US & Canada)"
 
-    # Overwrite Rails errors to use Bootstrap CSS classes
-    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
-      "<span class=\"has-error\">#{html_tag}</span>".html_safe
-    end
+    # Ignores custom error DOM elements created by Rails.
+    config.action_view.field_error_proc = proc { |html_tag, _instance| html_tag }
   end
 end
