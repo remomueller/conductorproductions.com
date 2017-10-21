a7bc13fbc4c407e3d65e8aae07ea7db91094ff85
Remo Mueller <remosm@gmail.com>
2017-10-21 16:55:24 -0400

    Migrate to new gems.rb format

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,12 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listed in gems.rb, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module WwwConductorproductionsCom
+  # Conductor provides videos, a client portal, and an administrative backend.
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
